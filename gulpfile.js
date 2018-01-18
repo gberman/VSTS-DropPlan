295398e8939c684b87aeec4278ec436b5b84a2e7
Gregory Berman <gregory.berman@gmail.com>
2018-01-18 00:40:21 -0500

    updated gulp, and index template

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var gulp       = require('gulp'),
+let gulp       = require('gulp'),
     rename     = require("gulp-rename"),
     concat     = require('gulp-concat'),
     uglify     = require('gulp-uglify'),
@@ -10,9 +10,9 @@ var gulp       = require('gulp'),
     livereload = require('gulp-livereload'),
     replace    = require('gulp-replace'),
     copy       = require('gulp-copy'),
-    template   = require('gulp-template');
+    del        = require('del');
 
-var css = {
+let css = {
     sourceFiles: ['styles/main.css', 'styles/jquery-ui.css'],
     fileName: 'dropPlan',
     environment: {
@@ -27,7 +27,7 @@ var css = {
     }
 };
 
-var js = {
+let js = {
     sourceFiles: ["scripts/Promise.js"
     ,"scripts/jquery-1.12.1.min.js"
     ,"scripts/jquery-ui.min.js"
@@ -53,93 +53,99 @@ var js = {
     }
 };
 
-gulp.task('concat-js', function(){
-    return gulp.src(js.sourceFiles)
-    .pipe(concat(js.fileName + js.environment.dev.extension))
-    .pipe(gulp.dest(js.environment.dev.path))
-    .pipe(livereload());
-});
+let Development = {
+    Scripts: function(){
+        return gulp.src(js.sourceFiles)
+          .pipe(concat(js.fileName + js.environment.dev.extension))
+          .pipe(gulp.dest(js.environment.dev.path))
+          .pipe(livereload());
+    },
+    Styles: function(){
+        return gulp.src(css.sourceFiles)
+          .pipe(concat(css.fileName + css.environment.dev.extension))
+          .pipe(gulp.dest(css.environment.dev.path))
+          .pipe(livereload());
+    },
+    Env: 'dev'
+};
+let Production = {
+    Scripts: function(){
+        return gulp.src([
+            js.environment.prod.sourceFiles[0]
+          , js.environment.prod.sourceFiles[1]
+          , js.environment.dev.path + js.fileName + js.environment.dev.extension])
+          .pipe(concat(js.fileName + js.environment.prod.extension))
+          .pipe(sourcemaps.init({loadMaps: true}))
+          .pipe(uglify())
+          .pipe(sourcemaps.write('.', {addComment: false}))
+          .pipe(gulp.dest(js.environment.prod.path));
+    },
+    Styles: function(){
+        return gulp.src([css.environment.dev.path + css.fileName + css.environment.dev.extension])
+          .pipe(rename({extname: css.environment.prod.extension}))
+          .pipe(gulp.dest(css.environment.prod.path));
+    },
+    Env: 'prod'
+};
 
-gulp.task('uglify-js', function(){
-    return gulp.src([
-          js.environment.prod.sourceFiles[0]
-        , js.environment.prod.sourceFiles[1]
-        , js.environment.dev.path + js.fileName + js.environment.dev.extension])
-      .pipe(concat(js.fileName + js.environment.prod.extension))
-      .pipe(sourcemaps.init({loadMaps: true}))
-      .pipe(uglify())
-      .pipe(sourcemaps.write('.', {addComment: false}))
-      .pipe(gulp.dest(js.environment.prod.path));
-});
-
-gulp.task('concat-css', function(){
-    return gulp.src(css.sourceFiles)
-    .pipe(concat(css.fileName + css.environment.dev.extension))
-    .pipe(gulp.dest(css.environment.dev.path))
-    .pipe(livereload());
-});
-
-gulp.task('copy-css', function(){
-    return gulp.src([css.environment.dev.path + css.fileName + css.environment.dev.extension])
-      .pipe(rename({extname: css.environment.prod.extension}))
-      .pipe(gulp.dest(css.environment.prod.path));
-});
-
-gulp.task('resource:dev', ['resource-css:dev', 'resource-static:dev', 'resource-html']);
-gulp.task('resource:prod', ['resource-css:prod', 'resource-static:prod', 'resource-html', 'minified-resource-html']);
-gulp.task('vsts:dev', function(){
-    return gulp.src(['vss-extension-test.json'])
-        .pipe(replace('{{URL}}', 'localhost:8080'))
-        .pipe(rename('vss-extension.json'))
-        .pipe(gulp.dest('./dist/dev/'));
-});
-gulp.task('vsts:prod', function(){
-    return gulp.src(['vss-extension-real.json'])
-        .pipe(rename('vss-extension.json'))
-        .pipe(gulp.dest('./dist/prod/'));
-});
-gulp.task('resource-css:dev', function(){
-    return gulp.src(['styles/*.css', '!styles/jquery-ui.css'])
-        .pipe(copy('./dist/dev/'));
-});
-gulp.task('resource-css:prod', function(){
-    return gulp.src(['styles/*.css', '!styles/jquery-ui.css'])
-        .pipe(copy('./dist/prod/'));
-});
-gulp.task('resource-static:dev', function(){
-    return gulp.src(['images/*', 'README.md', 'LICENSE'])
-        .pipe(copy('./dist/dev/'));
-});
-gulp.task('resource-static:prod', function(){
-    return gulp.src(['images/*', 'README.md', 'LICENSE'])
-        .pipe(copy('./dist/prod/'));
-});
-
-gulp.task('resource-html', function(){
-    return gulp.src(['index.html'])
-        .pipe(replace(/#{now}/g, new Date().toJSON()))
-        .pipe(gulp.dest('./dist/dev'));
-});
-
-gulp.task('minified-resource-html', function(){
-    return gulp.src(['./dist/dev/index.html'])
-        .pipe(replace('#{isMinified}', ''))
-        .pipe(gulp.dest('./dist/prod'));
-});
-
-
-
-gulp.task('dev-resources', ['concat-js', 'concat-css', 'resource:dev', 'vsts:dev']);
-gulp.task('prod-resources', ['uglify-js', 'copy-css', 'resource:prod', 'vsts:prod']);
-
-gulp.task('default', ['dev-resources'], function(){gulp.run('prod-resources')});
-
-gulp.task('watch', ['dev-resources'], function(done){
+function clean(){
+    return del('dist');
+}
+function copyStaticFiles(env){
+    return function CopyEnvStaticFiles(){
+        return gulp.src(['styles/*.css', '!styles/jquery-ui.css',
+                        'images/*',
+                        'README.md',
+                        'LICENSE'])
+        .pipe(copy('./dist/' + env + '/'));
+    }
+}
+function copyDynamicFiles(env, templateData){
+    return function BuildAndCopyDynamicFiles(){
+        let task = gulp.src(['index.html', 'vss-extension.json'])  
+            
+        templateData.forEach(function(data, index){
+            task.pipe(replace(data.Key, data.Value));
+        });
+        task.pipe(gulp.dest('./dist/' + env));
+        return task;
+    }
+}
+exports.watch = function(done){
     livereload.listen();
-    gulp.watch('scripts/*.js', ['concat-js']);
-    gulp.watch('styles/*.css', ['concat-css', 'resource-css:dev']);
+    gulp.watch('scripts/*.js', Development.Scripts);
+    gulp.watch('styles/*.css', function(){Development.Styles(); copyStaticFiles(Development.Env)();});
 
     http.createServer(
-        st({ path: '.', index: 'index.html', cache: false })
+        st({ path: './dist/dev/', index: 'index.html', cache: false })
     ).listen(8080, done);
-});
+}
+exports.default = gulp.series(
+        clean, 
+        gulp.parallel(
+            Development.Styles, 
+            Development.Scripts,
+            copyStaticFiles(Development.Env),
+            copyStaticFiles(Production.Env),
+            copyDynamicFiles(Development.Env, [
+                {Key: '#{now}', Value: new Date().toJSON()},
+                {Key: '#{testing-flag}', Value: '-test'},
+                //{Key: '"public": false', Value: '"public": false'},
+                {Key: '"uri": "index.html"', Value: '"uri": "localhost:8080"'},
+                {Key: '#{isMinified}', Value: ''}
+            ]),
+            copyDynamicFiles(Production.Env, [
+                {Key: '#{now}', Value: new Date().toJSON()},
+                {Key: '#{testing-flag}', Value: ''},
+                {Key: '"public": false', Value: '"public": true'},
+                //{Key: '"uri": "index.html"', Value: '"uri": "index.html"'},
+                {Key: '#{isMinified}', Value: '.min'}
+            ])),
+            gulp.parallel(
+                Production.Scripts,
+                Production.Styles
+            ));
+
+exports.clean = clean
+exports.styles = Development.Styles
+exports.scripts = Development.Scripts
